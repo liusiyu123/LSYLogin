efe6061897abd306c09d3d0e21043bab8e5d7e9e
liusiyu <1641594428@qq.com>
2021-04-21 18:36:57 +0800

    test1

---
+++ LSYLogin/Classes/LSYLoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LSYLoginViewController.h
+//  LSYLogin_Example
+//
+//  Created by liusiyu on 2021/4/21.
+//  Copyright © 2021 liusiyu123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSYLoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LSYLogin/Classes/LSYLoginViewController.m
@@ -0,0 +1,33 @@
+//
+//  LSYLoginViewController.m
+//  LSYLogin_Example
+//
+//  Created by liusiyu on 2021/4/21.
+//  Copyright © 2021 liusiyu123. All rights reserved.
+//
+
+#import "LSYLoginViewController.h"
+
+@interface LSYLoginViewController ()
+
+@end
+
+@implementation LSYLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
